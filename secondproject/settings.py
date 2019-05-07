be00981b584bc3b09a360c69de52c4e40cac87b9
unknown <gkaqjawns@gmail.com>
2019-05-07 20:42:07 +0900

    헤로쿠 배포

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jyr=_zyi)#w)a1@^+flr2i4^-60u8#wfxy*0dzsohq_yfwn!-7'
-
+#SECRET_KEY = 'jyr=_zyi)#w)a1@^+flr2i4^-60u8#wfxy*0dzsohq_yfwn!-7'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'jyr=_zyi)#w)a1@^+flr2i4^-60u8#wfxy*0dzsohq_yfwn!-7')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
